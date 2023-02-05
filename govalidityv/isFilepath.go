bb296515d65fbe457ce0d65750c92616c085c6c8
Saeed Ghanbari <sgh370@yahoo.com>
2023-02-05 12:36:35 +0330

    fix

--- govalidityv/isFilepath.go
+++ govalidityv/isFilepath.go
@@ -9,7 +9,7 @@ func IsFilepath(field string, params ...interface{}) (bool, error) {
 	label, value := GetFieldLabelAndValue(field, params)
 	err := GetErrorMessageByFieldValue(govaliditym.Default.IsFilepath, label, value)
 	str := value.(string)
-	rxFilepath := regexp.MustCompile("^(?:[-+]?[0-9]+)?(?:[.][0-9]+)?(?:[a-zA-Z0-9]+)?(?:[.][a-zA-Z0-9]+)?$")
+	rxFilepath := regexp.MustCompile("^(\\/.*?)[\\.][\\w]{1,}$")
 	isValid := rxFilepath.MatchString(str)
 	if isValid {
 		return true, nil
